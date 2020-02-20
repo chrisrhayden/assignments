ac487450145a39bf43dfa204000ee3b7ee8a0e5b
chris <chrisrhayden@gmail.com>
2020-02-19 23:17:43 -0800

    general clean up and small fixes

--- cs163/three/src/table.cpp
+++ cs163/three/src/table.cpp
@@ -7,46 +7,61 @@ using namespace std;
 #include "list.h"
 #include "table.h"
 
+// prototype so we can define it below and use it in the  class's and not add
+// it to the table.h so it cant be imported by the outside
+
+// parse_line and add the tokens to the right struct members
 bool parse_line(ToParse *to_parse, char *&new_key, HouseData *to_fill);
+// the basic hash function used by the table class
+long int hash_function(int table_size, char *key);
 
-long int hash_function(char *key, int table_size) {
-    // lol, just for now
-    return key[0] % table_size;
-}
-
+// the table constructor
 Table::Table(int size) {
-    table = new List *[size];
+    // set the table size
+    table_size = size;
 
+    // make a new list
+    table = new List *[table_size];
+
+    // make a list pointer
     List **current = table;
 
+    // iterate over the while table and set each index to null
     for (int i = 0; i < size; ++i) {
+        // set the current index to null
         current = 0;
+        // get to the next table index or list node
         ++current;
     }
 
-    table_size = size;
     return;
 }
 
+// the table deconstructor
 Table::~Table() {
+    // if no table then were good to go
     if (!table) {
         return;
     }
 
+    // get a list pointer
     List **current = table;
 
+    // iterate to end of table
     for (int i = 0; i < table_size; ++i) {
+        // remove List class
         delete *current;
+        // get to the next index / list class
         ++current;
     }
 
-    // delete *current;
-
+    // finally free the array
     delete[] table;
 }
 
+// add to the hash table
 bool Table::add(char *key, HouseData *to_add) {
-    long int hash = hash_function(key, table_size);
+    long int hash = hash_function(table_size, key);
 
     if (!table[hash]) {
         List *list = new List();
@@ -56,13 +71,17 @@ bool Table::add(char *key, HouseData *to_add) {
     return table[hash]->add(hash, to_add);
 }
 
+// display all items in the table from the beginning of the array to the end,
+// mostly for debugging
 bool Table::display_all() {
     List **current = table;
 
     bool success = true;
 
     for (int i = 0; i < table_size && success; ++i) {
+        // dereference to get to the node
         if (*current) {
+            // if there is data then ask it to display itself
             success = (*current)->display();
         }
 
@@ -72,6 +91,7 @@ bool Table::display_all() {
     return success;
 }
 
+// fill the table from a file
 bool Table::fill_data_from_file(char *path) {
     int max_size = 1000;
     // get a handle on the input file
@@ -91,8 +111,8 @@ bool Table::fill_data_from_file(char *path) {
 
     bool success = true;
 
-    // while its not the end of the file, we can still get data and the parsing
-    // worked last time
+    // while its not the end of the file, we can still get data and the
+    // parsing worked last time
     while (input_file.peek() && !input_file.eof() && success) {
         input_file.getline(to_parse->source, to_parse->max_size);
 
@@ -111,8 +131,6 @@ bool Table::fill_data_from_file(char *path) {
 bool Table::parse_line_and_add_to_data(ToParse *to_parse) {
     // ignore any line starting with a #
     if (to_parse->source[0] == '#') {
-        // clear the temp array more or less
-        to_parse->source[0] = '\0';
         return true;
     }
 
@@ -137,6 +155,11 @@ bool Table::parse_line_and_add_to_data(ToParse *to_parse) {
     return success;
 }
 
+long int hash_function(int table_size, char *key) {
+    // lol, just for now
+    return key[0] % table_size;
+}
+
 // these functions kinda suck but oh well ---------->
 ToParse::ToParse(int size) {
     source = new char[size];
@@ -149,14 +172,17 @@ ToParse::~ToParse() {
     delete[] source;
 }
 
-bool add_if_exists(char *source, char *&to_fill) {
-    if (source) {
-        to_fill = new char[strlen(source)];
-        strcpy(to_fill, source);
-        return true;
+// add source to dest but safely, revers c order because I dont like c's way
+bool strcpy_safe(char *source, char *&dest) {
+    // if source and it has data then
+    if (!source && source[0] == '\0') {
+        return false;
     }
 
-    return false;
+    dest = new char[strlen(source)];
+    // c is fucking stupid
+    strcpy(dest, source);
+    return dest[0] != '\0';
 }
 
 bool parse_line(ToParse *to_parse, char *&new_key, HouseData *to_fill) {
@@ -164,98 +190,41 @@ bool parse_line(ToParse *to_parse, char *&new_key, HouseData *to_fill) {
 
     char delimiter[] = "$";
 
+    // this function is kinda confusing but
+    // first call strtok with a string to tokenize and a delimiter
+    // then every subsequent call to strtok with null instead of a string
+    // will give a pointer to a new array with the next token from the
+    // string I guess strtok will clean everything up, idk
     char *split = strtok(to_parse->source, delimiter);
 
-    success = add_if_exists(split, new_key);
+    success = strcpy_safe(split, new_key);
 
     if (success) {
         split = strtok(0, delimiter);
-        success = add_if_exists(split, to_fill->location);
+        success = strcpy_safe(split, to_fill->location);
     }
 
     if (success) {
         split = strtok(0, delimiter);
-        success = add_if_exists(split, to_fill->size);
+        success = strcpy_safe(split, to_fill->size);
     }
 
     if (success) {
         split = strtok(0, delimiter);
-        success = add_if_exists(split, to_fill->bedrooms);
+        success = strcpy_safe(split, to_fill->bedrooms);
     }
 
     if (success) {
         split = strtok(0, delimiter);
-        success = add_if_exists(split, to_fill->distance_to_psu);
+        success = strcpy_safe(split, to_fill->distance_to_psu);
     }
 
     if (success) {
         split = strtok(0, delimiter);
-        success = add_if_exists(split, to_fill->notes);
+        success = strcpy_safe(split, to_fill->notes);
     }
 
     // TODO: maybe do a few checks if the data is correct
+    // explicitly check for false to be clear
     return to_fill->is_empty() == false;
 }
-
-// fill a sting until a pattern
-// int fill_from_str(ToParse *to_parse, char *&to_fill, int stop) {
-//     if (stop == -1) {
-//         return -1;
-//     }
-//
-//     // make a temporary array to hold the data from the source string
-//     char *temp = new char[to_parse->max_size];
-//     // get the full source length as a default stopping condition
-//     int parse_len = strlen(to_parse->source);
-//
-//     // instantiate i and target outside the loop to use later in the function
-//     int i = 0;
-//     int end = stop;
-//     // fill a given sting hard code a pattern for ease of use
-//     for (; i < parse_len && to_parse->source[end] != '$'; ++i, ++end) {
-//         temp[i] = to_parse->source[end];
-//     }
-//
-//     // if i is 0 then noting was done and so an error is in the file
-//     if (i == 0) {
-//         end = -1;
-//
-//     } else {
-//         // cap off the string
-//         temp[i] = '\0';
-//
-//         // now make a perfectly fitting array for the segment
-//         to_fill = new char[strlen(temp)];
-//
-//         // copy the segment in to the given array
-//         strcpy(to_fill, temp);
-//     }
-//
-//     // remove the temporary array
-//     delete[] temp;
-//
-//     // return new end
-//     return end;
-// }
-//
-// bool parse_and_add(ToParse *to_parse, char *&new_key, HouseData *to_fill) {
-//     int stopped = 0;
-//
-//     char delimiter[] = "$";
-//
-//     char *split = strtok(to_parse->source, delimiter);
-//
-//     // fill the key first
-//     stopped = fill_from_str(to_parse, new_key, stopped);
-//     // add 1 to the stopped variable to skip the last $ char
-//     stopped = fill_from_str(to_parse, to_fill->distance_to_psu, stopped + 1);
-//     stopped = fill_from_str(to_parse, to_fill->size, stopped + 1);
-//     stopped = fill_from_str(to_parse, to_fill->bedrooms, stopped + 1);
-//     stopped = fill_from_str(to_parse, to_fill->location, stopped + 1);
-//     stopped = fill_from_str(to_parse, to_fill->notes, stopped + 1);
-//     // TODO: maybe do a few checks if the data is correct
-//
-//     return stopped;
-// }
-
-// <----------
