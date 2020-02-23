6d9dc81ed089263eb580e9a5d32d4ade01df9b2b
chris <chrisrhayden@gmail.com>
2020-02-23 06:19:00 -0800

    got data in test_data; memory leaks :sad:

--- cs163/three/src/table.h
+++ cs163/three/src/table.h
@@ -11,7 +11,7 @@ class Table {
     bool get(char *key, MealData *to_fill);
     bool remove(char *key);
     bool remove(char *key, MealData *to_fill);
-    int retrieve(DataKey data_key, char *substr, MealData **meal_arry_to_fill);
+    int retrieve(DataKey data_key, char *substr, MealData ***meal_arry_to_fill);
     bool display_all();
 
   private:
