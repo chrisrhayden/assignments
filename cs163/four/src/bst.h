184442443b3f339766a861a84ae90fd7179ba56a
chris <chrisrhayden@gmail.com>
2020-03-09 06:37:01 -0700

    update h file

--- cs163/four/src/bst.h
+++ cs163/four/src/bst.h
@@ -1,5 +1,7 @@
+struct MealData;
+
 struct Node {
-    struct MealData *data;
+    MealData *data;
     Node *left;
     Node *right;
 };
@@ -8,13 +10,13 @@ class Tree {
   public:
     Tree();
     ~Tree();
-    bool add();
-    bool remove(char *meal_name, char *restaurant);
-    int remove_all();
-    bool display(char *meal_name);
-    bool display_all();
     int get_height();
     bool is_efficient();
+    bool display(char *meal_name);
+    bool display_all();
+    bool add(MealData *new_data);
+    bool remove(char *meal_name, char *restaurant);
+    int remove_all();
 
   private:
     int remove_all(Node *current);
